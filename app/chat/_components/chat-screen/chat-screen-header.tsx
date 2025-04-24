aab0ec4bbb314dabe0f4f3c1e09a73ae38a5ae37
daggle-minseok <ms.lee@daggle.io>
2025-04-24 19:52:23 +0900

    feat : dialog 추가

--- app/chat/_components/chat-screen/chat-screen-header.tsx
+++ app/chat/_components/chat-screen/chat-screen-header.tsx
@@ -5,8 +5,64 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import GuideBox from "../guide/guide-box";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useState } from "react";
 
 const ChatScreenHeader = () => {
+  const [showMobileGuide, setShowMobileGuide] = useState(false);
+
+  const guideContents = [
+    {
+      title: "일반 대화",
+      items: [
+        {
+          title: "메뉴 추천",
+          description: "지금 계절에 맞는 제철음식을 추천해줘",
+        },
+        {
+          title: "날씨 확인",
+          description: "오늘 날씨링 미세먼지 농도 알려줘",
+        },
+        {
+          title: "힐링 정리",
+          description: "오늘 힐링 투두 리스트 만들어줘",
+        },
+      ],
+    },
+    {
+      title: "날씨 확인",
+      items: [
+        {
+          title: "일기 예보",
+          description: "이번 주 날씨는 어떨까?",
+        },
+        {
+          title: "미세먼지",
+          description: "오늘 마스크 써야할까?",
+        },
+      ],
+    },
+    {
+      title: "힐링 정리",
+      items: [
+        {
+          title: "투두리스트",
+          description: "오늘의 할일을 정리해볼까?",
+        },
+        {
+          title: "일정관리",
+          description: "이번주 일정을 관리해줘",
+        },
+      ],
+    },
+  ];
+
   return (
     <div className="py-5 px-8 flex justify-between items-center">
       <p className="text-body-l font-hakgyo-ansim">Chat AI 오피스</p>
@@ -26,9 +82,18 @@ const ChatScreenHeader = () => {
         </Popover>
       </div>
       <div className="block web:hidden">
-        <button className="flex items-center justify-center">
-          <CircleHelp className="size-6 text-primary" />
-        </button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <button className="flex items-center justify-center">
+              <CircleHelp className="size-6 text-primary" />
+            </button>
+          </DialogTrigger>
+          <DialogContent className="p-6">
+            <DialogHeader>
+              <DialogTitle>Chat AI 오피스 가이드</DialogTitle>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
