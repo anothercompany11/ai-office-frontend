c31edbdec3dd1d4a0e18ba826749efe74c410dd8
amisu1203 <doh_0112@naver.com>
2025-04-24 14:43:34 +0900

    feat: 로그인 페이지 ui

---
+++ app/shared/logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Logo = () => {
+  return (
+    <Link href={"/"}>
+      <Image src={"/svg/logo.svg"} alt="AI 오피스" width={100} height={21} />
+    </Link>
+  );
+};
+
+export default Logo;
