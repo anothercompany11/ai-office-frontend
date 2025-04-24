5177e07168417f8091e62d7c79df92df1de8059e
daggle-minseok <ms.lee@daggle.io>
2025-04-24 17:54:01 +0900

    feat : 브레이크 포인트 추가

--- app/chat/_components/chat-screen/chat-screen-header.tsx
+++ app/chat/_components/chat-screen/chat-screen-header.tsx
@@ -10,19 +10,26 @@ const ChatScreenHeader = () => {
   return (
     <div className="py-5 px-8 flex justify-between items-center">
       <p className="text-body-l font-hakgyo-ansim">Chat AI 오피스</p>
-      <Popover>
-        <PopoverTrigger asChild>
-          <button className="flex items-center justify-center">
-            <CircleHelp className="size-6 text-primary" />
-          </button>
-        </PopoverTrigger>
-        <PopoverContent
-          className="w-[640px] p-6 bg-white border border-[hsla(220,10%,94%,1)] shadow-[4px_4px_20px_0px_hsla(0,0%,0%,0.1)]"
-          align="end"
-        >
-          <GuideBox />
-        </PopoverContent>
-      </Popover>
+      <div className="hidden web:block">
+        <Popover>
+          <PopoverTrigger asChild>
+            <button className="flex items-center justify-center">
+              <CircleHelp className="size-6 text-primary" />
+            </button>
+          </PopoverTrigger>
+          <PopoverContent
+            className="w-[640px] p-6 bg-white border border-[hsla(220,10%,94%,1)] shadow-[4px_4px_20px_0px_hsla(0,0%,0%,0.1)]"
+            align="end"
+          >
+            <GuideBox />
+          </PopoverContent>
+        </Popover>
+      </div>
+      <div className="block web:hidden">
+        <button className="flex items-center justify-center">
+          <CircleHelp className="size-6 text-primary" />
+        </button>
+      </div>
     </div>
   );
 };
